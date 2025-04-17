86644109e45653fff747cd5bc1473d5aaa636638
Rishu Singh <rishusingh2902@gmail.com>
2025-04-17 22:12:48 +0530

    Update api.js

--- frontend1/src/config/api.js
+++ frontend1/src/config/api.js
@@ -1,9 +1,9 @@
 export const API_CONFIG = {
-    BASE_URL: "http://localhost:8000/api/v1",
+    BASE_URL: "https://naukri-project.up.railway.app/api/v1", // Replace localhost with live URL
     ENDPOINTS: {
       USER: {
         LOGIN: "/user/login",
         REGISTER: "/user/register"
       }
     }
-  };
+};
